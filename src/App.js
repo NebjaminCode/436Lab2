4c18904646e8ee9d4bc56ca39e312a90c1973e7c
Ben Code <bjamescode@gmail.com>
2022-09-29 13:48:26 -0500

    second commit, still getting basics down from class demo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+import UserBar from './Components/User/UserBar'
+
+function App() {
+
+  return (
+    <div>
+      <UserBar />
+    </div>
+  );
+}
+
+export default App;
